7f34e3fe53f6527d040009f0bc9743929a782827
Thandokazi Ronica Dumezweni <150345700+Mutombene@users.noreply.github.com>
2025-11-10 08:24:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double hoursWorked = 0.0;            //input
+            double rate = 0.0;                  //input
+            double salary = 0.0;                //output
+
+            Console.WriteLine("Enter hours worked");
+            hoursWorked = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter rate of pay");
+            rate =Convert.ToDouble(Console.ReadLine());
+
+            //program logic
+            salary = hoursWorked * rate;
+
+            Console.WriteLine("Salary is \tR" + salary);
+
+        }
+    }
+}
